168740cf996ead54fafb3f92cf4c16c927400f4f
Elizabeth B. Clouser-Kuhn <14024239+elizabethclouser@users.noreply.github.com>
2026-07-18 16:01:50 -0400

    chore: [Issue-31] CLI: Migrate eslint configuration to support ESM

---
+++ packages/cli/eslint.config.js
@@ -0,0 +1,73 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import prettier from "eslint-plugin-prettier";
+import packageJson from "eslint-plugin-package-json";
+import markdownlintConfig from '../../.markdownlint.json' with { type: 'json' };
+import jsoncEslintParser from 'jsonc-eslint-parser';
+import typescriptEslintParser from '@typescript-eslint/parser';
+import eslintPluginMarkdownlintParser from 'eslint-plugin-markdownlint/parser';
+
+export default defineConfig([
+    globalIgnores(['node_modules/', 'dist/', 'coverage/', '/reports']),
+    {
+      files: ['**.md'],
+      languageOptions: {
+        parser: eslintPluginMarkdownlintParser,
+      },
+      /**
+       * eslint-plugin-markdownlint currently does not support configuration files. We currently
+       * configure markdownlint using the markdownlint.json file.
+       *
+       * @see    https://https://www.npmjs.com/package/eslint-plugin-markdownlint, Limitations section
+       * @author Elizabeth B. Clouser-Kuhn <clouser.elizabeth@protonmail.com>
+       */
+      rules: Object.entries(markdownlintConfig).filter(([_k, v]) => !v).reduce((a, [b]) => {
+        return Object.assign(a, { [`markdownlint/${b.toLowerCase()}`]: 'off' });
+      }, {})
+    },
+    {
+      plugins: {prettier},
+      files: ['src/**/*.ts', 'assembly/**/*.ts'],
+      languageOptions: {
+        parser: typescriptEslintParser,
+        // parserOptions: {
+        //   project: ['./tsconfig.json'],
+        // },
+        globals: {
+          browser: true,
+          es2020: true,
+          jest: true,
+        },
+      },
+      rules: {
+        '@typescript-eslint/no-var-requires': 'off',
+        'object-curly-newline': 'off',
+        'operator-linebreak': 'off',
+        'no-confusing-arrow': 'off',
+        'implicit-arrow-linebreak': 'off',
+        'function-paren-newline': 'off',
+        'no-unsafe-optional-chaining': 'off',
+        'no-useless-escape': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        'class-methods-use-this': 'off',
+        'no-use-before-define': 'off',
+        'no-shadow': 'off',
+      },
+    },
+    {
+      languageOptions: {
+        parserOptions: {
+          extraFileExtensions: ['.json'],
+        },
+        parser: jsoncEslintParser,
+      },
+      extends: [packageJson.configs.recommended],
+      files: ['package.json'],
+      plugins: {packageJson},
+      rules: {
+        'package-json/require-description': 'off',
+        'package-json/valid-version': 'error',
+        'package-json/require-license': 'off',
+        'package-json/require-type': 'off',
+      },
+    },
+]);
